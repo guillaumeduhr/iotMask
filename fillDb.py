b2292bfe5792f34b25a586eac4117a5ff2398b9b
guillaumeduhr <guillaume.duhr@gmail.com>
2021-12-01 10:05:01 +0100

    filldb

---
+++ fillDb.py
@@ -0,0 +1,13 @@
+import time
+import sqlite3
+from datetime import datetime
+
+con = sqlite3.connect('example.db')
+cur = con.cursor()
+
+while True:
+    cur.execute("insert into test values (?, ?)", (datetime.now(), True))
+    con.commit()
+    time.sleep(.5)
+
+con.close()
